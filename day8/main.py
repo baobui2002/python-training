59ea322fb6be0fc27100606445374c15fb78fc28
baobui2002 <baobuif8@gmail.com>
2024-05-03 20:58:28 +0700

    change code

---
+++ day8/main.py
@@ -0,0 +1,14 @@
+"""
+list | tuple | set | dict comprehensions
+"""
+new = []
+
+for i in range(5):
+    new.append(i**2)
+
+print(i)
+print(new)
+
+
+""" new = [x**2 for x in range(5)]
+print(new) """
